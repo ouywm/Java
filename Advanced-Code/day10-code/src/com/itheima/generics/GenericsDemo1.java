e478e2d4fea8d4e613c3beed0eb068562a28acc2
欧阳维民 <113845776+oywmwoaini520@users.noreply.github.com>
2023-12-19 17:24:35 +0800

    2023/12/19

---
+++ Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo1.java
@@ -0,0 +1,10 @@
+package com.itheima.generics;
+
+public class GenericsDemo1 {
+	/*
+
+	 */
+	public static void main(String[] args) {
+
+	}
+}
--- Advanced-Code/day10-code/src/com/itheima/test/Test.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.test;
-
-public class Test {
-	public static void main(String[] args) {
-
-	}
-}

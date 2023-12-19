ca8a2a9aed9b0751e5bc35321b07ad75e0e13cc9
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-19 20:08:01 +0800

    2023/12/19

--- Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo4.java
+++ Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo4.java
@@ -5,8 +5,19 @@ public class GenericsDemo4 {
 		泛型接口
 	 */
 
-
 	public static void main(String[] args) {
+
+
+
+	}
+}
+interface Inter<E>{
+	void show(E e);
+}
+
+class InterAImpl implements Inter<String>{
+	@Override
+	public void show(String s) {
 
 	}
 }

0cb0b2439f053d890df0dfe3252d35606f02e9a3
欧阳维民 <113845776+oywmwoaini520@users.noreply.github.com>
2023-12-19 17:26:38 +0800

    2023/12/19

--- Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo1.java
+++ Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo1.java
@@ -2,9 +2,16 @@ package com.itheima.generics;
 
 public class GenericsDemo1 {
 	/*
+		泛型介绍 : JDK5引入的，可以在编译阶段约束操作的数据类型，并进行检查
+
+		泛型的好处 :
+					1.统一数据类型
+					2.将运行期间的错误提升到了编译期
 
 	 */
 	public static void main(String[] args) {
+
+
 
 	}
 }

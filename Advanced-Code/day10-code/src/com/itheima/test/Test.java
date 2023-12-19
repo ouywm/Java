e566582525f26387e72095b5952a834a881ceebe
欧阳维民 <113845776+oywmwoaini520@users.noreply.github.com>
2023-12-19 11:12:45 +0800

    2023/12/19

--- Advanced-Code/day10-code/src/com/itheima/test/Test.java
+++ Advanced-Code/day10-code/src/com/itheima/test/Test.java
@@ -2,6 +2,9 @@ package com.itheima.test;
 
 public class Test {
 	public static void main(String[] args) {
-
+		print();
+	}
+	public static void print(){
+		System.out.println("Hello World!");
 	}
 }

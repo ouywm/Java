3afaabd646dc4797187dd6118161a593bc39b247
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-19 20:31:45 +0800

    2023/12/19

--- Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo4.java
+++ Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo4.java
@@ -3,21 +3,33 @@ package com.itheima.generics;
 public class GenericsDemo4 {
 	/*
 		泛型接口
+
+				1.实现类，实现接口的时候确定到接口的具体类型
+				2.实现类实现接口，没有指定具体类型，就让接口的泛型，跟着类的泛型去匹配
 	 */
 
 	public static void main(String[] args) {
-
-
+		InterBImpl<String> i = new InterBImpl<>();
 
 	}
 }
-interface Inter<E>{
+//泛型接口
+interface Inter<E> {
 	void show(E e);
 }
 
-class InterAImpl implements Inter<String>{
+//实现类想好给什么类型了
+class InterAImpl implements Inter<String> {
 	@Override
 	public void show(String s) {
+
+	}
+}
+
+//实现类也没想好要给什么类型，那就到创建对象的时候再给类型吧,没想好那就跟着类的泛型走
+class InterBImpl<E> implements Inter<E> {
+	@Override
+	public void show(E e) {
 
 	}
 }
---
+++ Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo5.java
@@ -0,0 +1,68 @@
+package com.itheima.generics;
+
+public class GenericsDemo5 {
+	/*
+		泛型通配符
+	 */
+	public static void main(String[] args) {
+
+
+	}
+}
+
+abstract class Employee {
+	private String name;
+	private double salary;
+
+	public Employee() {
+	}
+
+	public Employee(String name, double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+
+	/**
+	 * 获取
+	 *
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * 设置
+	 *
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * 获取
+	 *
+	 * @return salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
+
+	/**
+	 * 设置
+	 *
+	 */
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String toString() {
+		return "Employee{name = " + name + ", salary = " + salary + "}";
+	}
+}
+
+class Coder extends Employee {
+}
+
+class Manager extends Employee {
+}

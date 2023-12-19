8feec3987ae774e28947f23e22cda3145af094c5
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-19 19:18:15 +0800

    2023/12/19

---
+++ Advanced-Code/day10-code/src/com/itheima/generics/GenericsDemo2.java
@@ -0,0 +1,26 @@
+package com.itheima.generics;
+
+public class GenericsDemo2 {
+	/*
+
+		常见的泛型表示符 : E V K T
+
+			E : Element 表示元素
+			T : Type 表示类型
+			K : ket 表示键
+			V : Value 表示值
+
+		泛型类演示
+	 */
+
+	public static void main(String[] args) {
+
+
+
+	}
+}
+
+
+class Student<E>{
+
+}

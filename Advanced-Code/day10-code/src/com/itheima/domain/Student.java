a8ef79c021aade215233a0b80f81ffb07b0fbef8
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-21 20:15:33 +0800

    2023/12/21

--- Advanced-Code/day10-code/src/com/itheima/domain/Student.java
+++ Advanced-Code/day10-code/src/com/itheima/domain/Student.java
@@ -1,25 +1,26 @@
 package com.itheima.domain;
 
-/**
+/*
  * 这里的泛型表示，要比较的对象，接口的泛型就跟着类走呗
  * 这个接口有个泛型，因为人家也不知道你要拿啥类型的来做比较，
  * 就随便你了呗，由你决定哈哈
- * @param <Student>
  */
-public class Student<Student> implements Comparable<Student> {
+public class Student implements Comparable<Student> {
 
-	/**
-	 * 每一次调用add添加方法的时候，都会调用compareTo方法，看他的返回值是啥，
-	 * 如果是0就是不添加元素，表示里面有相同的元素不添加，但是第一个添加的元素是根节点，
-	 * 就只添加一个，然后后面添加的元素就会跟他作比较，因为一添加就会调用compareTo这个方法，
-	 * 这里的返回值如果是0就表示都是一样的，有相同的元素就不添加了呗！正数1就是正序输出，
-	 * -1就是倒序输出
-	 * @param o the object to be compared.
-	 * @return 返回值影响添加
+	/*
+	  每一次调用add添加方法的时候，都会调用compareTo方法，看他的返回值是啥，
+	  如果是0就是不添加元素，表示里面有相同的元素不添加，但是第一个添加的元素是根节点，
+	  就只添加一个，然后后面添加的元素就会跟他作比较，因为一添加就会调用compareTo这个方法，
+	  这里的返回值如果是0就表示都是一样的，有相同的元素就不添加了呗！正数1就是正序输出，
+	  -1就是倒序输出
+	  返回值影响添加
 	 */
 	@Override
 	public int compareTo(Student o) {
-		return 0;
+		// 0 表示一样的，一样的就不添加
+		// 1 表示是比根节点更大的值，就比右边大了，都往右边走，输出我们看到的就是正序
+		// -1 就是比根节点更小的喽，往左边走，所以我们看到的就是倒序
+		return this.age - o.age;
 	}
 
 	private String name;
@@ -30,6 +31,7 @@ public class Student<Student> implements Comparable<Student> {
 
 	/**
 	 * 姓名跟年龄呗！！
+	 *
 	 * @param name
 	 * @param age
 	 */
@@ -75,7 +77,6 @@ public class Student<Student> implements Comparable<Student> {
 	}
 
 	/**
-	 *
 	 * @return 打印对象名，就不会看到地址值了，因为重写了toString
 	 */
 	public String toString() {
--- Advanced-Code/day10-code/src/com/itheima/set/TerrSetDemo2.java
+++ Advanced-Code/day10-code/src/com/itheima/set/TerrSetDemo2.java
@@ -8,14 +8,15 @@ public class TerrSetDemo2 {
 	/*
 		TreeSet集合储存Student学生对象
 
+		取出的顺序 : 左，中，右
+
 	 */
 	public static void main(String[] args) {
 		TreeSet<Student> ts = new TreeSet<>();
-		ts.add(new Student("王五",25));
-		ts.add(new Student("张三",23));
-		ts.add(new Student("李四",22));
-		ts.add(new Student("赵六",20));
-
+		ts.add(new Student("王五", 25));
+		ts.add(new Student("张三", 23));
+		ts.add(new Student("李四", 22));
+		ts.add(new Student("赵六", 20));
 		System.out.println(ts);
 
 	}

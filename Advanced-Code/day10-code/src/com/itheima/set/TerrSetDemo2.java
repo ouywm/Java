927c917c3530200154a4390a7140bde7f67a5a4b
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-21 17:11:04 +0800

    2023/12/21

--- Advanced-Code/day10-code/src/com/itheima/set/TerrSetDemo2.java
+++ Advanced-Code/day10-code/src/com/itheima/set/TerrSetDemo2.java
@@ -1,6 +1,10 @@
 package com.itheima.set;
 
 public class TerrSetDemo2 {
+	/*
+		TreeSet集合储存Student学生对象
+
+	 */
 	public static void main(String[] args) {
 
 	}
--- Advanced-Code/day10-code/src/com/itheima/set/TreeSetDemo1.java
+++ Advanced-Code/day10-code/src/com/itheima/set/TreeSetDemo1.java
@@ -1,10 +1,23 @@
 package com.itheima.set;
 
+import java.util.TreeSet;
+
 public class TreeSetDemo1 {
 	/*
-
+		TreeSet集合的特点体验 : 排序，去重
 	 */
 	public static void main(String[] args) {
+
+		TreeSet<String> ts = new TreeSet<String>();
+		ts.add("d");
+		ts.add("c");
+		ts.add("a");
+		ts.add("d");
+		ts.add("b");
+		ts.add("d");
+		ts.add("e");
+		ts.add("d");
+		System.out.println(ts);
 
 	}
 }

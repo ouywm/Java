6f2eafa95b864a04d130e42cb04483a87a72d94c
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-22 10:42:45 +0800

    2023/12/21

--- Advanced-Code/day11-code/src/com/itheima/hashset/StringDemo.java
+++ Advanced-Code/day11-code/src/com/itheima/hashset/StringDemo.java
@@ -1,4 +1,20 @@
 package com.itheima.hashset;
 
 public class StringDemo {
+	/*
+		问题 ： 该如何重写hashCode方法?
+		回答 : 应该将类的索引属性，参与到哈希值的计算当中
+				只有这样做,(哈希值冲突)的概率，才会比较小
+	 */
+
+	public static void main(String[] args) {
+
+		//String类重写过hashCode方法，是根据字符串的每一个字符进行计算
+
+		System.out.println("通话".hashCode());
+
+		System.out.println("重地".hashCode());
+
+		// 但是也会有相同的哈希值出现，就要加上equals方法比较内容
+	}
 }

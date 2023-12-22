e5e480369ff50ebfc85a72a464c76dd20fe3c7e3
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-22 11:01:03 +0800

    2023/12/21

--- Advanced-Code/day11-code/src/com/itheima/hashset/StringDemo.java
+++ Advanced-Code/day11-code/src/com/itheima/hashset/StringDemo.java
@@ -5,6 +5,17 @@ public class StringDemo {
 		问题 ： 该如何重写hashCode方法?
 		回答 : 应该将类的索引属性，参与到哈希值的计算当中
 				只有这样做,(哈希值冲突)的概率，才会比较小
+
+		我们在打印对象名的时候，在没有重写toString方法的时候，会打印一个地址值，
+		这个地址值像下面这个toString，没有被重写的时候是这样的
+
+		public String toString() {
+            return getClass().getName() + "@" + Integer.toHexString(hashCode());
+            // 全类名+@+通过Integer调用toHexString(传值hashCode()方法，返回一个值)，转换成十六进制所谓的地址值
+            //本质来讲就是一个哈希值
+        }
+
+        重写之后，我们打印的就不是地址值了
 	 */
 
 	public static void main(String[] args) {

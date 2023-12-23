ee08057f97b7b66dd4ded81289b75192b6415d9d
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-23 21:59:11 +0800

    2023/12/23

--- Advanced-Code/day10-code/src/com/itheima/set/TreeSetDemo1.java
+++ Advanced-Code/day10-code/src/com/itheima/set/TreeSetDemo1.java
@@ -7,6 +7,10 @@ public class TreeSetDemo1 {
 		TreeSet集合的特点体验 : 排序，去重
 
 		下面的就是去掉重复的了，还有排了序
+
+		为啥有去重的效果？？？
+		因为TreeSet集合在调用add方法添加的时候，会自动调用一个compareTo()这样的方法，
+		会有返回值，负数代表比她小，0就代表是一样的，正数就代表比它大
 	 */
 	public static void main(String[] args) {
 

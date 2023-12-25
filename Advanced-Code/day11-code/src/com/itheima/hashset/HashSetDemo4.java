273f75b24da331d73d314531c94a207deb4cfbe6
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-25 09:36:19 +0800

    2023/12/25

--- Advanced-Code/day11-code/src/com/itheima/hashset/HashSetDemo4.java
+++ Advanced-Code/day11-code/src/com/itheima/hashset/HashSetDemo4.java
@@ -3,6 +3,27 @@ package com.itheima.hashset;
 import java.util.HashSet;
 
 public class HashSetDemo4 {
+
+	/*
+		TREEIFY_THRESHOLD代表着阈值8
+
+		for (int binCount = 0; ; ++binCount) {
+                    if ((e = p.next) == null) {
+                        p代表着当前元素p.next是获取下一个意思
+                        p.next = newNode(hash, key, value, null);
+                        判断binCount这个值是否大于等于这个阈值-1，为啥是-1，因为-1是不是01234567，binCount是从0开始也是01234567也是8个，
+                        大于或者等于7，是1的时候操作的其实是数组中的第二个元素因为p.next获取下一个了然后到第9个元素才是第八个，
+                        treeifyBin(tab, hash);就是树化操作，
+                        if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
+                            treeifyBin(tab, hash);
+                        break;
+                    }
+                    if (e.hash == hash &&
+                        ((k = e.key) == key || (key != null && key.equals(k))))
+                        break;
+                    p = e;
+                }
+	 */
 	public static void main(String[] args) {
 		HashSet<String> hs = new HashSet<>();
 

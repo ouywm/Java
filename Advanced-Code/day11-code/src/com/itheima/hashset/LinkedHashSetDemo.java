2a3fc0232b530198adf389b80fc8282c93bcd105
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-25 20:34:07 +0800

    2023/12/25

--- Advanced-Code/day11-code/src/com/itheima/hashset/LinkedHashSetDemo.java
+++ Advanced-Code/day11-code/src/com/itheima/hashset/LinkedHashSetDemo.java
@@ -1,13 +1,23 @@
 package com.itheima.hashset;
 
+import java.util.LinkedHashSet;
+
 public class LinkedHashSetDemo {
 
 	/*
 		LinkedHashSet特点 : 去重，并保证存取顺序
+
+		双向链表，在添加元素的时候，上一个元素会下一个元素的地址，下一个元素也会记录上一个的元素地址
 	 */
 	public static void main(String[] args) {
 
+		LinkedHashSet<String> lhs = new LinkedHashSet<>();
 
-
+		lhs.add("c");
+		lhs.add("d");
+		lhs.add("b");
+		lhs.add("a");
+		lhs.add("c");
+		System.out.println(lhs);
 	}
 }

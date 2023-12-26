4a50d0cd24fdcb037bb88a80e67d1e36e4dc8170
欧阳维民 <113845776+ouywm@users.noreply.github.com>
2023-12-26 08:55:54 +0800

    2023/12/25

---
+++ Advanced-Code/day11-code/src/com/itheima/args/ArgsDemo.java
@@ -0,0 +1,7 @@
+package com.itheima.args;
+
+public class ArgsDemo {
+	public static void main(String[] args) {
+
+	}
+}
--- Advanced-Code/day11-code/src/com/itheima/hashset/LinkedHashSetDemo.java
+++ Advanced-Code/day11-code/src/com/itheima/hashset/LinkedHashSetDemo.java
@@ -8,6 +8,7 @@ public class LinkedHashSetDemo {
 		LinkedHashSet特点 : 去重，并保证存取顺序
 
 		双向链表，在添加元素的时候，上一个元素会下一个元素的地址，下一个元素也会记录上一个的元素地址
+		这就是元素存取顺序，还有去重，去重倒是早就知道为啥能去重了
 	 */
 	public static void main(String[] args) {
 

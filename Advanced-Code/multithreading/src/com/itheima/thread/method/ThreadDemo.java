e309f803ff0b0e89fd0babfd9d27e6e550bb1d80
ouywm <2656771275@qq.com>
2024-02-01 19:39:36 +0800

    2024/2/1

---
+++ Advanced-Code/multithreading/src/com/itheima/thread/method/ThreadDemo.java
@@ -0,0 +1,13 @@
+package com.itheima.thread.method;
+
+public class ThreadDemo {
+	/*
+		String getName()                     返回次线程的名称
+		void setName(String name)            设置线程的名名称
+		static Thread currentThread()        获取当前的对象
+		static void sleep(long time)         让线程休眠指定的时间，单位为毫秒
+	 */
+	public static void main(String[] args) {
+
+	}
+}
